4158674ce0571ff9d5c16eb710dee21149938a44
lucasscabral <lucas.9cabral@gmail.com>
2024-05-24 01:17:54 -0300

    feature(CDK-21): implementa as páginas do projeto

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,36 @@
+import CardDataStats from "@/Components/CardDataStats";
+import DefaultLayout from "@/Layouts/DefaultLayout";
+import { IoPeopleOutline } from "react-icons/io5";
+
+export default function Home({}) {
+  return (
+    <DefaultLayout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardDataStats title="Total de Eleitores" total="1000">
+          <svg
+            className="fill-primary dark:fill-white"
+            width="22"
+            height="18"
+            viewBox="0 0 22 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <IoPeopleOutline size={22} color="#3C50E0" />
+          </svg>
+        </CardDataStats>
+        <CardDataStats title="Total de Liderança" total="10">
+          <svg
+            className="fill-primary dark:fill-white"
+            width="22"
+            height="18"
+            viewBox="0 0 22 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <IoPeopleOutline size={22} color="#3C50E0" />
+          </svg>
+        </CardDataStats>
+      </div>
+    </DefaultLayout>
+  );
+}
