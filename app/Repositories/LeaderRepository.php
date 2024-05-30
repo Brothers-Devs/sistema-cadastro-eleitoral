0dd52299af30a06833943da61359ffe8db1d895b
cabralti <cabral.9santos@gmail.com>
2024-05-30 16:23:58 -0300

    fix(CDK-26): ajustes reuniao 30-05
    - [front] ajusta o nome dos botoes da tela de Cadastro e Editar para 'Salvar'
    - [front] ajusta label para do 'Nome do Eleitor'
    - [back] possibilidade de alterar o nome do lider

--- app/Repositories/LeaderRepository.php
+++ app/Repositories/LeaderRepository.php
@@ -11,12 +11,23 @@ class LeaderRepository
 {
     public function __construct(
         public Leader $model
-    ) {
+    )
+    {
     }
 
     public function all()
     {
         return $this->model->all();
+    }
+
+    public function findByCpf(string $cpf)
+    {
+        $leader = $this->model->where('cpf', $cpf)->first();
+        if (!$leader) {
+            return null;
+        }
+
+        return $leader;
     }
 
     public function create(CreateLeaderDto $dto)
