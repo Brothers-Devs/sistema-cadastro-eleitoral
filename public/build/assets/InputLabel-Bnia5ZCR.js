69466377acf970c2d2e6a6a7dd8051757ea58a39
lucasscabral <lucas.9cabral@gmail.com>
2024-05-30 17:19:54 -0300

    buiild alterações

--- public/build/assets/InputLabel-Bnia5ZCR.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputLabel-BezGHCvn.js
-import{j as m}from"./app-CkBP6Bbs.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
-========
-import{j as m}from"./app-BWCG_Cv1.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
->>>>>>>> main:public/build/assets/InputLabel-Bnia5ZCR.js
