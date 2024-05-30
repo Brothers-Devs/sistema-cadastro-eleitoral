8c93886f82dfacfeb2ad6e2b2214cefc88587edb
cabralti <cabral.9santos@gmail.com>
2024-05-30 00:26:41 -0300

    fix: remove mascara antes das validacoes
    - campo cpf deve ser unico na tabela de voters

--- app/Http/Requests/StoreUpdateVoterRequest.php
+++ app/Http/Requests/StoreUpdateVoterRequest.php
@@ -27,27 +27,41 @@ class StoreUpdateVoterRequest extends FormRequest
             'name' => 'required|max:255',
             'date_of_birth' => ['nullable', new DateFormatPtBr()],
             'rg' => 'nullable|max:10',
-            'cpf' => 'required|size:14|unique:voters',
-            'phone' => 'required|size:16',
+            'cpf' => 'required|size:11|unique:voters',
+            'phone' => 'required|size:11',
             'title_number' => 'nullable|max:45',
             'zone' => 'nullable|max:45',
             'session' => 'nullable|max:5',
             'address' => 'nullable|string|max:255',
-            'zip_code' => 'nullable|size:9',
+            'zip_code' => 'nullable|size:8',
             'neighborhood' => 'nullable|string|max:255',
             'city' => 'nullable|string|max:100',
             'leader_name' => 'required|max:255',
-            'leader_cpf' => 'required|size:14'
+            'leader_cpf' => 'required|size:11'
         ];
 
         if ($this->method() == 'PUT' || $this->method() == 'PATCH') {
             $rules['cpf'] = [
                 'required',
-                'size:14',
-                Rule::unique('voters')->ignore($this->voter_id)
+                'size:11',
+                Rule::unique('voters')->ignore($this->id)
             ];
         }
 
         return $rules;
     }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'cpf' => preg_replace('/\D/', '', $this->cpf),
+            'phone' => preg_replace('/\D/', '', $this->phone),
+            'zip_code' => preg_replace('/\D/', '', $this->zip_code),
+            'leader_cpf' => preg_replace('/\D/', '', $this->leader_cpf),
+        ]);
+    }
+
 }
