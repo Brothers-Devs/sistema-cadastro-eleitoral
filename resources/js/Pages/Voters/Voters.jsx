374f71a5283fc1e486a362db002204cfb8cca301
lucasscabral <lucas.9cabral@gmail.com>
2024-06-05 17:09:18 -0300

    Squashed merge branch CDK-18

--- resources/js/Pages/Voters/Voters.jsx
+++ resources/js/Pages/Voters/Voters.jsx
@@ -17,128 +17,126 @@ export default function Voters({ items, leaders }) {
   const [openModal, setOpenModal] = useState(false);
   const [rowSelected, setRowSelected] = useState(false);
 
-  // const handleClose = () => {
-  //   setOpenModal(false);
-  // };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
 
-  // const columns = useMemo(
-  //   () => [
-  //     {
-  //       field: "id",
-  //       headerName: "ID",
-  //       width: 80,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //     },
-  //     {
-  //       field: "name",
-  //       headerName: "Nome",
-  //       width: 250,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //     },
-  //     {
-  //       field: "cpf",
-  //       headerName: "CPF",
-  //       width: 130,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       disableClickEventBubbling: true,
-  //       valueGetter: (_, row) => {
-  //         return `${mask(row?.cpf, PATTERN_CPF)}`;
-  //       },
-  //     },
-  //     {
-  //       field: "phone",
-  //       headerName: "Telefone",
-  //       width: 140,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       valueGetter: (_, row) => {
-  //         return `${mask(row?.phone, PATTERN_PHONE)}`;
-  //       },
-  //     },
-  //     {
-  //       field: "city",
-  //       headerName: "Município",
-  //       width: 120,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       valueGetter: (_, row) => {
-  //         return `${row?.city ? row.city : "-"}`;
-  //       },
-  //     },
-  //     {
-  //       field: "neighborhood",
-  //       headerName: "Bairro",
-  //       width: 120,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       valueGetter: (_, row) => {
-  //         return `${row?.neighborhood ? row.neighborhood : "-"}`;
-  //       },
-  //     },
+  const columns = useMemo(
+    () => [
+      {
+        field: "id",
+        headerName: "ID",
+        width: 80,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+      },
+      {
+        field: "name",
+        headerName: "Nome",
+        width: 250,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+      },
+      {
+        field: "cpf",
+        headerName: "CPF",
+        width: 130,
+        headerClassName: "bg-bodydark2 text-white",
+        disableClickEventBubbling: true,
+        valueGetter: (_, row) => {
+          return `${mask(row?.cpf, PATTERN_CPF)}`;
+        },
+      },
+      {
+        field: "phone",
+        headerName: "Telefone",
+        width: 140,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+        valueGetter: (_, row) => {
+          return `${mask(row?.phone, PATTERN_PHONE)}`;
+        },
+      },
+      {
+        field: "city",
+        headerName: "Município",
+        width: 120,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+        valueGetter: (_, row) => {
+          return `${row?.city ? row.city : "-"}`;
+        },
+      },
+      {
+        field: "neighborhood",
+        headerName: "Bairro",
+        width: 120,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+        valueGetter: (_, row) => {
+          return `${row?.neighborhood ? row.neighborhood : "-"}`;
+        },
+      },
 
-  //     {
-  //       field: "leader",
-  //       headerName: "Liderança",
-  //       width: 200,
-  //       disableClickEventBubbling: true,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       valueGetter: (_, row) => {
-  //         return `${row?.leader.name} (${mask(row?.leader.cpf, PATTERN_CPF)})`;
-  //       },
-  //     },
-  //     {
-  //       headerName: "Ações",
-  //       width: 108,
-  //       headerClassName: "bg-bodydark2 text-white",
-  //       renderCell: ({ row }) => (
-  //         <ButtonsActions
-  //           rowSelected={row}
-  //           modification={modification}
-  //           setModification={setModification}
-  //         />
-  //       ),
+      {
+        field: "leader",
+        headerName: "Liderança",
+        width: 200,
+        disableClickEventBubbling: true,
+        headerClassName: "bg-bodydark2 text-white",
+        valueGetter: (_, row) => {
+          return `${row?.leader.name} (${mask(row?.leader.cpf, PATTERN_CPF)})`;
+        },
+      },
+      {
+        headerName: "Ações",
+        width: 108,
+        headerClassName: "bg-bodydark2 text-white",
+        renderCell: ({ row }) => (
+          <ButtonsActions
+            rowSelected={row}
+            modification={modification}
+            setModification={setModification}
+          />
+        ),
 
-  //       sortable: false,
-  //       filterable: false,
-  //       disableClickEventBubbling: true,
-  //     },
-  //   ],
-  //   [rowSelected, modification]
-  // );
+        sortable: false,
+        filterable: false,
+        disableClickEventBubbling: true,
+      },
+    ],
+    [rowSelected, modification]
+  );
 
-  // return (
-  //   <DefaultLayout>
-  //     <Breadcrumb pageName="Eleitores" />
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Eleitores" />
 
-  //     <div className="flex justify-between">
-  //       <div></div>
-  //       <PrimaryButton
-  //         className="bg-green-600 text-slate-50 hover:bg-green-500 mb-10 h-11"
-  //         onClick={() => setOpenModal(!openModal)}
-  //       >
-  //         <Tooltip title="Cadastrar Eleitor">Cadastrar</Tooltip>
-  //       </PrimaryButton>
-  //     </div>
+      <div className="flex justify-between">
+        <div></div>
+        <PrimaryButton
+          className="bg-green-600 text-slate-50 hover:bg-green-500 mb-10 h-11"
+          onClick={() => setOpenModal(!openModal)}
+        >
+          <Tooltip title="Cadastrar Eleitor">Cadastrar</Tooltip>
+        </PrimaryButton>
+      </div>
 
-  //     {/* Lista de Eleitores */}
-  //     <DataGridUtils
-  //       dataContent={items}
-  //       columns={columns}
-  //       setRowSelected={setRowSelected}
-  //     />
+      {/* Lista de Eleitores */}
+      <DataGridUtils
+        dataContent={items}
+        columns={columns}
+        setRowSelected={setRowSelected}
+      />
 
-  //     {/* Modal de Cadastro de Eleitores */}
-  //     <ModalCreateVoter
-  //       showModal={openModal}
-  //       handleClose={handleClose}
-  //       title={"Cadastrar Eleitor"}
-  //       typeButton={"Salvar"}
-  //       leaders={leaders}
-  //     />
-  //   </DefaultLayout>
-  // );
-
-  return <SendWhatsapp leaders={leaders} />;
+      {/* Modal de Cadastro de Eleitores */}
+      <ModalCreateVoter
+        showModal={openModal}
+        handleClose={handleClose}
+        title={"Cadastrar Eleitor"}
+        typeButton={"Salvar"}
+        leaders={leaders}
+      />
+    </DefaultLayout>
+  );
 }
