ef0cd2b40aabbd610117a28d22094311d2b7261d
lucasscabral <lucas.9cabral@gmail.com>
2024-08-30 19:29:54 -0300

    fix(CDK-39):Retira as colunas da raiz do componente

--- resources/js/Pages/Leaders/Leaders.jsx
+++ resources/js/Pages/Leaders/Leaders.jsx
@@ -4,12 +4,8 @@ import DefaultLayout from "@/Layouts/DefaultLayout";
 import DataGridUtils from "@/Utils/DataGridUtils";
 import { Tooltip } from "@mui/material";
 import { useMemo, useState } from "react";
-import ButtonsActions from "./Actions/ButtonActions";
-import { mask } from "remask";
 import ModalCreateLeader from "./Modal/ModalCreateLeader";
-import dayjs from "dayjs";
-
-const PATTERN_CPF = ["999.999.999-99"];
+import columnsLeaders from "./Columns/columnsLeaders";
 
 export default function Leaders({ items }) {
   const [modification, setModification] = useState(false);
@@ -21,70 +17,7 @@ export default function Leaders({ items }) {
   };
 
   const columns = useMemo(
-    () => [
-      {
-        field: "id",
-        headerName: "ID",
-        width: 80,
-        disableClickEventBubbling: true,
-        headerClassName: "bg-bodydark2 text-white",
-      },
-      {
-        field: "name",
-        headerName: "Nome da Liderança",
-        width: 270,
-        disableClickEventBubbling: true,
-        headerClassName: "bg-bodydark2 text-white",
-      },
-      {
-        field: "cpf",
-        headerName: "CPF",
-        width: 150,
-        disableClickEventBubbling: true,
-        headerClassName: "bg-bodydark2 text-white",
-        valueGetter: (_, row) => {
-          return `${mask(row?.cpf, PATTERN_CPF)}`;
-        },
-      },
-      {
-        field: "voters",
-        headerName: "Total de Eleitores",
-        width: 150,
-        sortable: true,
-        disableClickEventBubbling: true,
-        headerClassName: "bg-bodydark2 text-white",
-        valueGetter: (_, row) => {
-          return Number(row?.voters.length);
-        },
-        align: "center",
-      },
-      {
-        field: "created_at",
-        headerName: "Data de Cadastro",
-        width: 180,
-        disableClickEventBubbling: true,
-        headerClassName: "bg-bodydark2 text-white",
-        valueGetter: (_, row) => {
-          return `${dayjs(row?.created_at).format("DD/MM/YYYY HH:mm")}`;
-        },
-      },
-      {
-        headerName: "Ações",
-        width: 319,
-        headerClassName: "bg-bodydark2 text-white",
-        renderCell: ({ row }) => (
-          <ButtonsActions
-            rowSelected={row}
-            modification={modification}
-            setModification={setModification}
-          />
-        ),
-
-        sortable: false,
-        filterable: false,
-        disableClickEventBubbling: true,
-      },
-    ],
+    () => columnsLeaders(modification, setModification),
     [rowSelected, modification]
   );
 
